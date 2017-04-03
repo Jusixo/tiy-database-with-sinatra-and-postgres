812b3e7c5038a7a73297a7134c387a05a95665ee
Justin Valentin <Justinvalentine88@gmail.com>
2017-04-03 18:18:16 -0400

    fixed broken code

--- tiydatabase.rb
+++ tiydatabase.rb
@@ -13,10 +13,10 @@ get '/employees' do
   erb :employees
 end
 
-get '/employee_page' do
+get '/employee' do
   select_employee = params["name"]
 
-  database = PG.connect(dbname["tiy-database"])
+  database = PG.connect(dbname: "tiy-database")
   @people = database.exec("SELECT * FROM employees WHERE name = $1", [select_employee])
 
   erb :employee_page
@@ -26,15 +26,14 @@ get '/new_employee' do
   erb :new_employee
 end
 
-get 'create_employee' do
-  erb :create_employee
-  name = params ["name"]
-  phone = params ["phone"]
-  address = params ["address"]
-  position = params ["position"]
-  salary = params ["salary"]
-  slack = params ["slack"]
-  github = params ["guthub"]
+get '/create_employee' do
+  name = params["name"]
+  phone = params["phone"]
+  address = params["address"]
+  position = params["position"]
+  salary.to_i = params["salary"]
+  slack = params["slack"]
+  github = params["guthub"]
 
   database = PG.connect(dbname: "tiy-database")
   @accounts = database.exec("INSERT INTO employees(name, phone, address, position, salary, github, slack) VALUES($1, $2, $3, $4, $5, $6, $7)", [name, phone, address, position, salary, github, slack])
@@ -67,7 +66,7 @@ get '/update_employee' do
   phone = params["phone"]
   address = params["address"]
   position = params["position"]
-  salary = params["salary"]
+  salary.to_i = params["salary"]
   github = params["github"]
   slack = params["slack"]
   id = params["id"]
